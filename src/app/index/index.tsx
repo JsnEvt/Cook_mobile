8ad3a244f23907ae688bcc908d4a6f37320bcece
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-03-05 09:17:15 -0300

    Estrutura inicial - primeira interface

---
+++ src/app/index/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from 'react-native';
+import { styles } from "./styles"
+import { Ingredients } from '@/components/Ingredients';
+import { useState } from 'react';
+
+
+export default function Index() {
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Escolha {"\n"}
+        <Text style={styles.subtitle}>os produtos</Text>
+      </Text>
+      <Text style={styles.message}>Descubra receitas baseadas nos produtos que você escolheu.</Text>
+      <Ingredients />
+    </View>
+  )
+}
